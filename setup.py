ffdca1d28144f1f5d52301b29115408f7e301183
Tim Pyrkov <tim.pyrkov@gmail.com>
2023-02-16 18:01:41 +0100

    Activity cutoff operator '>=' -> '>'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="pynhanes",
-    version="0.0.10",
+    version="0.0.11",
     author="Tim Pyrkov",
     author_email="tim.pyrkov@gmail.com",
     description="Python parser and scraper for NHANES accelerometry and questionnaire",
